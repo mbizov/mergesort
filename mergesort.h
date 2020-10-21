40e23f152ac47807b8e4fb6bf5566140db95f2ce
marin <34997944+mbizov@users.noreply.github.com>
2020-10-21 09:56:59 +0300

    updated variable name

--- mergesort.h
+++ mergesort.h
@@ -1,34 +1,33 @@
 #ifndef _MERGESORT_H_
 #define _MERGESORT_H_
 
-void merge_sort(int a[], int aux[], int start, int end) {
+void merge_sort(int a[], int temp[], int start, int end) {
     if (end <= start) return;
     int mid = (start + end) / 2;
-
-    merge_sort(a, aux, start, mid);
-    merge_sort(a, aux, mid + 1, end);
-
     int left = start;
     int right = mid + 1;
     int k;
-
+    
+    merge_sort(a, temp, start, mid);
+    merge_sort(a, temp, mid + 1, end);
+    
     for (k = start; k <= end; k++) {
         if (left == mid + 1) {
-            aux[k] = a[right];
+            temp[k] = a[right];
             right++;
         } else if (right == end + 1) {
-            aux[k] = a[left];
+            temp[k] = a[left];
             left++;
         } else if (a[left] < a[right]) {
-            aux[k] = a[left];
+            temp[k] = a[left];
             left++;
         } else {
-            aux[k] = a[right];
+            temp[k] = a[right];
             right++;
         }
     }
 
-    for (k = start; k <= end; k++) a[k] = aux[k];
+    for (k = start; k <= end; k++) a[k] = temp[k];
 }
 
 #endif
